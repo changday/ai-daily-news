266447c3eb49ae724573d0365650a20c50564cb7
Chang Dai <changdai@ChangdeMac-mini.local>
2023-07-15 13:30:54 +0800

    homepage configuration

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "AI Daily News";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Welcome to Daily AI News, your source for the latest news and information about artificial intelligence.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "AI Daily News";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
